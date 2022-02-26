ef0793c6a994eb957cfd1203c48b703fd38460cc
Aiden <nagyeong.yeo1023@gmail.com>
2022-02-26 23:00:28 +0900

    Add audio player

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
     
     var counter = 60
     
@@ -24,14 +27,16 @@ class ViewController: UIViewController {
         
         timer.invalidate()
         progressBar.progress = 1
-        
+
         guard let hardness = sender.currentTitle else { return }
         
         hardnessNonOptional = hardness
-        
+    
         counter = eggTimes[hardness] ?? 0
         
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+        
+        titleLabel.text = hardness
     }
     
 
@@ -46,8 +51,16 @@ class ViewController: UIViewController {
             timer.invalidate() // !
             titleLabel.text = "Done"
             progressBar.progress = 0
+            playSound()
         }
         
 
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
     }
 }
